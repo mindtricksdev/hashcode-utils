cb459db207b15743cf6aadf7547429313a3c5c2c
Daniel Iancu <daniel.iancu@ro.ecrion.com>
2020-02-19 15:19:19 +0200

    Implement watcher frontend.

--- utils/watcher/background/index.js
+++ utils/watcher/background/index.js
@@ -1,8 +1,51 @@
+const chokidar = require("chokidar");
 const admin = require("firebase-admin");
 const serviceAccount = require("./private/hashcodeutils-99b5eae7c765.json");
+
+const NAME = process.argv[2] || "test";
+
+console.log("Started watching " + NAME + "...");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 let db = admin.firestore();
+
+chokidar
+  .watch(".", {
+    cwd: "../../../history"
+  })
+  .on("add", file => {
+    let [set, score, sets] = file.split("__");
+    score = parseInt(score);
+    console.log("Set " + set + " scored " + score);
+
+    let docRef = db.collection("users").doc(NAME + "_" + set);
+
+    docRef.get().then(doc => {
+      let prev = {
+        max: -1
+      };
+      if (!doc.exists) {
+      } else {
+        prev = doc.data();
+
+        if (prev.last === score) return;
+      }
+
+      const next = {
+        ...prev,
+        last: score,
+        focus: sets,
+        name: NAME
+      };
+
+      if (!prev.max) next.max = score;
+      if (score > prev.max) next.max = score;
+
+      docRef
+        .set(next)
+        .then(() => console.log(" Updated set " + set + " scored " + score));
+    });
+  });
--- utils/watcher/frontend/script.js
+++ utils/watcher/frontend/script.js
@@ -2,10 +2,57 @@ document.addEventListener("DOMContentLoaded", function() {
   const db = firebase.firestore();
 
   db.collection("users").onSnapshot(function(querySnapshot) {
-    var cities = [];
+    const users = [];
     querySnapshot.forEach(function(doc) {
-      cities.push(doc.data().name);
+      const userSet = doc.data();
+      const crtName = userSet.name;
+      const crtSet = doc.id.replace(crtName + "_", "");
+
+      let user = users.find(u => u.name === crtName);
+      if (!user) {
+        user = {
+          name: crtName,
+          sets: {}
+        };
+        users.push(user);
+      }
+
+      user.sets[crtSet] = userSet;
     });
-    console.log("Current cities in CA: ", cities.join(", "));
+
+    render(users);
   });
 });
+
+const render = users => {
+  const rootEl = document.getElementById("root");
+
+  rootEl.innerHTML = `<table>
+	<tr>
+		<td></td>
+		<td>A</td>
+		<td>B</td>
+		<td>C</td>
+		<td>D</td>
+		<td>E</td>
+	</tr>
+	${users.map(renderUserRow)}
+	</table>`;
+};
+
+const renderUserRow = user => {
+  return `<tr>
+		<td>${user.name}</td>
+		<td>${renderScoreCell(user.sets.a)}</td>
+		<td>${renderScoreCell(user.sets.b)}</td>
+		<td>${renderScoreCell(user.sets.c)}</td>
+		<td>${renderScoreCell(user.sets.d)}</td>
+		<td>${renderScoreCell(user.sets.e)}</td>
+	</tr>`;
+};
+
+const renderScoreCell = userSet => {
+  if (!userSet) return "-";
+
+  return `<div>${userSet.max}</div><div>${userSet.last}</div>`;
+};
